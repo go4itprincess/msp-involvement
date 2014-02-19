a7106e7d0ad91d86276859c8256ca5a8a977e086
basile-henry <basile.henry@hotmail.com>
2014-02-19 21:04:00 +0000

    commented javascript

--- polygons_scraper.py
+++ polygons_scraper.py
@@ -9,7 +9,15 @@ constituencies_string = urllib2.urlopen(req).read()
 constituencies = json.loads(constituencies_string, 'iso-8859-1')
 
 output = []
+fo = open("data/population.csv", "r")
+population = fo.read()
+fo.close()
 
+population = population.split('\n')
+pops = []
+for p in population:
+    p = p.split(',')
+    pops.append(p)
 
 for constituency in constituencies.values():
     id = constituency['id']
@@ -19,8 +27,18 @@ for constituency in constituencies.values():
     req = urllib2.Request(url)
 
     polygons_scraped = re.findall("<coordinates>(.+?)</coordinates>",urllib2.urlopen(req).read())
-
     polygons = []
+
+    url = "http://mapit.mysociety.org/area/" + str(id)
+    req = urllib2.Request(url)
+    code = json.loads(urllib2.urlopen(req).read())
+
+    pop = 0
+    for p in pops:
+        if str(code['codes']['gss']) == p[0]:
+            pop = int(p[1])
+            break
+
 
     for poly in polygons_scraped:
         coordinates = poly.split(' ')
@@ -38,7 +56,7 @@ for constituency in constituencies.values():
 
         polygons.append(polygon)
 
-    output.append({"name":name,"id":id,"polygon":polygons})
+    output.append({"name":name,"id":id,"polygon":polygons, 'population': pop})
     print name
 
 fo = open("testapp/static/constituencies_polygons.js", "w+")
--- testapp/static/map.js
+++ testapp/static/map.js
@@ -22,3 +22,119 @@ $.getJSON("constituencies_polygons.js", function (data) {
 });
 
 
+
+
+
+
+
+
+
+/*
+// control that shows state info on hover
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Population</h4>' +  (props ?
+        '<b>' + props.name + '</b><br />' + props.population + ' people'
+        : 'Hover over a constituency');
+};
+
+info.addTo(map);
+
+
+// get color depending on population value
+function getColor(d) {
+    return d > 1000 ? '#800026' :
+           d > 500  ? '#BD0026' :
+           d > 200  ? '#E31A1C' :
+           d > 100  ? '#FC4E2A' :
+           d > 50   ? '#FD8D3C' :
+           d > 20   ? '#FEB24C' :
+           d > 10   ? '#FED976' :
+                      '#FFEDA0';
+}
+
+function style(feature) {
+    return {
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7,
+        fillColor: getColor(feature.properties.density)
+    };
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera) {
+        layer.bringToFront();
+    }
+
+    info.update(layer.feature.properties);
+}
+
+var geojson;
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+geojson = L.geoJson(statesData, {
+    style: style,
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+        labels = [],
+        from, to;
+
+    for (var i = 0; i < grades.length; i++) {
+        from = grades[i];
+        to = grades[i + 1];
+
+        labels.push(
+            '<i style="background:' + getColor(from + 1) + '"></i> ' +
+            from + (to ? '&ndash;' + to : '+'));
+    }
+
+    div.innerHTML = labels.join('<br>');
+    return div;
+};
+
+legend.addTo(map);
+
+*/
