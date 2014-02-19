fc0ecc704e5780b374b03a8a16b5c71095694f5e
Margus Lind <modulo@c104-78.uvn.zone.eu>
2014-02-20 00:39:44 +0200

    ajax endpoint extended

--- testapp/__init__.py
+++ testapp/__init__.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import _mysql
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route("/")
 def hello():
@@ -32,7 +32,7 @@ def const_info(constituency):
             m.surname,
             m.party,
             m.url,
-            SUM(d.rank2004+d.rank2006*4+d.rank2009*15+d.rank2012*30)/COUNT(*)/50/6505*100 AS rank,
+            AVG(d.rank2004+d.rank2006*4+d.rank2009*15+d.rank2012*30)/50/6505*100 AS rank,
             m.total_interventions,
             m.avg_intervention_len,
             m.total_mentions_of_constituency,
@@ -64,6 +64,60 @@ def const_info(constituency):
         }
         result += [msp]
         row = r.fetch_row()
+    result = {'result':result}
+    return jsonify(result)
+
+
+@app.route("/stats/", methods=['POST', 'GET'])
+def get_stats():
+    print(request.form.getlist('categories[]'))
+    categories = request.form.getlist('categories[]')
+    fields = {'c_id':'c.id', 'c_name':'c.name'}
+
+    if ("rank" in categories) or (len(categories)==0):
+        fields['rank']="AVG(d.rank2004+d.rank2006*4+d.rank2009*15+d.rank2012*30)/50/6505*100 AS rank"
+
+    if ("total_interventions" in categories) or (len(categories)==0):
+        fields['total_interventions']="AVG(m.total_interventions)"
+
+    if ("avg_intervention_len" in categories) or (len(categories)==0):
+        fields['avg_intervention_len']="AVG(m.avg_intervention_len)"
+
+    if ("total_mentions_of_constituency" in categories) or (len(categories)==0):
+        fields['total_mentions_of_constituency']="AVG(m.total_mentions_of_constituency)"
+
+    if ("interventions_with_mention" in categories) or (len(categories)==0):
+        fields['interventions_with_mention']="AVG(m.interventions_with_mention)"
+
+    if ("mentions_percentage_of_total_text" in categories) or (len(categories)==0):
+        fields['mentions_percentage_of_total_text']="AVG(m.mentions_percentage_of_total_text)"
+
+    if ("percentage_of_interventions_with_mention" in categories) or (len(categories)==0):
+        fields['percentage_of_interventions_with_mention']="AVG(m.percentage_of_interventions_with_mention)"
+
+    db = _mysql.connect(host="localhost", user="ilwhack14", passwd="hackathon", db="ilwhack14")
+    db.query("""
+        SELECT
+            {fields}
+        FROM constituencies c
+        LEFT JOIN MSPs m ON c.name=m.constituency
+        LEFT JOIN datazones d ON c.id = d.constituency
+        GROUP BY c.id
+    """.format(fields=' , '.join(fields.values())))
+
+    r = db.use_result()
+    result = []
+    row = r.fetch_row()
+
+    while row:
+        row = row[0]
+        constituency = {}
+        for (i, field) in enumerate(fields.keys()):
+            constituency[field] = row[i]
+
+        result += [constituency]
+        row = r.fetch_row()
+
     result = {'result':result}
     return jsonify(result)
 
