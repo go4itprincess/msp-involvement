ad5da71ac108e1405a5cc0c332c28e0e0290efdc
Margus Lind <modulo@c104-78.uvn.zone.eu>
2014-02-20 00:40:33 +0200

    MSP words

---
+++ words2db.py
@@ -0,0 +1,22 @@
+import json
+import _mysql
+
+with open('data/msps_words.json', 'r') as content_file:
+    content = json.loads(content_file.read())
+
+db = _mysql.connect(host="localhost", user="ilwhack14", passwd="hackathon", db="ilwhack14")
+words =[]
+for person in content:
+    q = "INSERT INTO `MSP_words` (`MSP_id`, `word`, `weight`)  VALUES "
+
+    values = []
+
+    for (word, weight) in person["top_words"]:
+        values.append("('{0}', '{1}', '{2}' )".format(person["person_id"], word, weight))
+        words.append(word)
+
+    q += ' , '.join(values)
+
+    db.query(q)
+    print(person['name'] + " OK!")
+print(str(len(words)) + ' words added. Done')
