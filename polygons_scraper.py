70638a162ddb8743b4e63634ccc426c12ec9b161
Dan Ristea <dan.ristea@skyscanner.net>
2014-02-19 22:20:00 +0000

    generating geojson

--- polygons_scraper.py
+++ polygons_scraper.py
@@ -1,6 +1,7 @@
 import json
 import urllib2
 import re
+import geojson
 
 url = "http://mapit.mysociety.org/areas/SPC"
 req = urllib2.Request(url)
@@ -19,6 +20,7 @@ for p in population:
     p = p.split(',')
     pops.append(p)
 
+feature_list = []
 for constituency in constituencies.values():
     id = constituency['id']
     name = constituency['name']
@@ -26,7 +28,7 @@ for constituency in constituencies.values():
     url = "http://mapit.mysociety.org/area/" + str(id) + ".kml"
     req = urllib2.Request(url)
 
-    polygons_scraped = re.findall("<coordinates>(.+?)</coordinates>",urllib2.urlopen(req).read())
+    polygons_scraped = re.findall("<coordinates>(.+?)</coordinates>", urllib2.urlopen(req).read())
     polygons = []
 
     url = "http://mapit.mysociety.org/area/" + str(id)
@@ -39,28 +41,28 @@ for constituency in constituencies.values():
             pop = int(p[1])
             break
 
-
+    pointsList = []
+    print name
     for poly in polygons_scraped:
         coordinates = poly.split(' ')
 
-        polygon = []
         i = 0
-
+        points = []
         for coordinate in coordinates:
             coordinate = map(float,coordinate.split(',')[:2])
-            coordinate[0], coordinate[1] = coordinate[1], coordinate[0]
 
             if i%10 == 0:
-                polygon.append(coordinate)
+                point = [coordinate[1], coordinate[0]]
+                points.append(point)
             i += 1
+        pointsList.append(points)
+    multipolygon = geojson.MultiPolygon(coordinates=pointsList)
+    feature = geojson.Feature(id=id, geometry=multipolygon, properties={'population': pop, 'name': name})
+    feature_list.append(feature)
+feature_collection = geojson.FeatureCollection(features=feature_list)
 
-        polygons.append(polygon)
-
-    output.append({"name":name,"id":id,"polygon":polygons, 'population': pop})
-    print name
-
-fo = open("testapp/static/constituencies_polygons.js", "w+")
-fo.write(json.dumps(output))
+fo = open("testapp/static/constituencies_geojson.json", "w+")
+fo.write(geojson.dumps(feature_collection))
 fo.close()
 
 print "Done!"
--- testapp/__init__.py
+++ testapp/__init__.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 def hello():
     return app.send_static_file('index.html')
 
+
 @app.route("/<filename>.css")
 def get_css(filename):
     return app.send_static_file(filename + '.css')
@@ -15,6 +16,11 @@ def get_css(filename):
 @app.route("/<filename>.js")
 def get_js(filename):
     return app.send_static_file(filename + '.js')
+
+
+@app.route("/<filename>.json")
+def get_js(filename):
+    return app.send_static_file(filename + '.json')
 
 
 @app.route("/constituency/<string:constituency>")
