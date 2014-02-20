34ce74b2e52bea34521a21ebc978979f827fedb2
basile-henry <basile.henry@hotmail.com>
2014-02-20 16:14:56 +0000

    break everything

--- testapp/static/map.js
+++ testapp/static/map.js
@@ -3,6 +3,11 @@ var map = L.map('map', {
         zoom: 6,
         minZoom: 5
         });
+map.on({
+       zoomstart: onZoomStart,
+       zoomend: onZoomEnd,
+       move: onMove
+       });
 map.setMaxBounds(L.latLngBounds(L.latLng(50.0,-20.0),L.latLng(65.0,15.0)));
 
 // add an OpenStreetMap tile layer
@@ -40,7 +45,21 @@ function buttonClicked(num) {
     }
 }
 
+
 var request_data =  "{\"result\": [    {      \"surname\": \" Scott\",\"percentage_of_interventions_with_mention\": \"0.14208\",      \"total_mentions_of_constituency\": \"723\",\"words\": \"[[\\\"draft\\\",0.465356], [\\\"swinney\\\",0.454288],[\\\"finance\\\",0.421423], [\\\"order\\\",0.351362], [\\\"local\\\",0.302168], [\\\"item\\\",0.260115], [\\\"finance\\\",0.421423], [\\\"order\\\",0.351362], [\\\"local\\\",0.302168], [\\\"item\\\",0.260115], [\\\"motion\\\",0.247806], [\\\"business\\\",0.235882]]\",\"mentions_percentage_of_total_text\": \"0.00322376\",\"shit\": 1,      \"avg_intervention_len\": \"133.768\",      \"name\": \"John\",      \"rank_c\": \"53.956609546201\",      \"interventions_with_mention\": \"236\",      \"MSP_id\": \"14091\",      \"url\": \"http://www.scottish.parliament.uk/images/MSPs and office holders Session 4/JohnScottMSP20110509.JPG\",      \"total_interventions\": \"1661\",      \"party\": \"Scottish Conservative and Unionist Party\"    }  ]}";
+
+var calibrated = function (list) {
+    max = Math.max.apply(null,list);
+    min = Math.min.apply(null,list);
+    size = (max-min);
+
+    var ret = [];
+    for(var i = 0; i<list.length; i++){
+        ret.push(100*(list[i]-min)/size);
+    }
+
+    return ret;
+}
 
 
 var geojson = L.geoJson(constituencies, {
@@ -89,10 +108,14 @@ var InfoControl = L.Control.extend({
     onAdd: function (map) {
         this._div = L.DomUtil.create('div', 'info');
         this.update();
+        this.locked = false;
         return this._div;
     },
 
+    locked: false,
+
     update: function (results, constituency) {
+
         if (results) {
         sel_constituency = JSON.parse(results);
 
@@ -122,8 +145,9 @@ var InfoControl = L.Control.extend({
           .start();
 
         } else {
-         this._div.innerHTML = "";
+         this._div.innerHTML = "<p>Hover over a constituency.</p>";
         }
+
     }
 });
 
@@ -175,31 +199,60 @@ function highlightFeature(e) {
     }
     constituency = layer.feature.properties.name
 
-
-    $.ajax({url:"/constituency/" + constituency, success: function(result) {
-        info.update(result, constituency);
-        }
-    });
-    info.update(request_data, constituency); //test purposes
+    if (!info.locked) {
+        $.ajax({url:"/constituency/" + constituency, success: function(result) {
+            info.update(result, constituency);
+            }
+        });
+        info.update(request_data, constituency); //test purposes
+    }
 
 }
 
 
 
 function resetHighlight(e) {
-    geojson.resetStyle(e.target);
-    info.update();
+
+
+    if (!info.locked) {
+        geojson.resetStyle(e.target);
+        info.update();
+    }
 }
 
 function onClick(e) {
+
     map.fitBounds(e.target.getBounds());
+    info.locked = false;
+    highlightFeature(e);
+    info.locked = true;
+
+//    info.locked = false;
+//    var defer = $.Deferred(), deferred_fit = defer.then(map.fitBounds);
+//    defer.resolve(e.target.getBounds());
+//    deferred_fit.done(function() {
+//    highlightFeature(e);info.locked = true});
+}
+
+function onZoomEnd(e) {
+    console.log("zoom ended");
+}
+
+function onZoomStart(e) {
+    info.locked = false;
+    console.log("zoom changed");
+}
+
+function onMove(e) {
+    info.locked = false;
+    console.log("map moved");
 }
 
 function onEachFeature(feature, layer) {
     layer.on({
         mouseover: highlightFeature,
         mouseout: resetHighlight,
-        click: onClick
+        click: onClick,
     });
 }
 
