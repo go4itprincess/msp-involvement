3c65188030940a5db6d9cd93cd91cd090f368e47
Margus Lind <margus.lind@gmail.com>
2014-02-21 02:53:16 +0000

    stuff

--- testapp/__init__.py
+++ testapp/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask, jsonify, request
 import _mysql
+import os
+from os.path import join, getsize
 
 app = Flask(__name__)
 
 # DB connection
 db = _mysql.connect(host="d33028.mysql.zone.ee", user="d33028sa73662", passwd="Hackath0n", db="d33028sd72729")
+
+def checkCache(name):
+    cache = name
+    if cache == 'Ayr':
+        return True
+    else:
+        return False
 
 @app.route("/")
 def hello():
@@ -21,20 +30,23 @@ def get_js(filename):
 @app.route("/<filename>.json")
 def get_json(filename):
     return app.send_static_file(filename + '.json')
-	
+
 @app.route("/<filename>.ico")
 def get_ico(filename):
     return app.send_static_file(filename + '.ico')
 
 @app.route("/constituency/<string:constituency>")
 def const_info(constituency):
+    # if checkCache(constituency):
+    #     return app.send_static_file('cache/' + constituency)
+
     db.query("""
         SELECT
             m.name,
             m.surname,
             m.party,
             m.url,
-            AVG(s_gen.2006*1+s_gen.2009*2+s_gen.2012*7)/6505*10 AS rank_gen,
+            (AVG(s_gen.2006*1+s_gen.2009*2+s_gen.2012*7)/6505*10-18)/67*100 AS rank_gen,
             m.total_interventions,
             m.avg_intervention_len,
             m.total_mentions_of_constituency,
@@ -43,20 +55,20 @@ def const_info(constituency):
             m.percentage_of_interventions_with_mention,
             m.id,
             GROUP_CONCAT(DISTINCT CONCAT("[\\\"", w.word, "\\\",", w.weight, "]") SEPARATOR ', '),
-            (AVG(s_cri.2006*1+s_cri.2009*2+s_cri.2012*7)/6505*10-9)/84*100 AS rank_cri,
-            (AVG(s_ed.2006*1+s_ed.2009*2+s_ed.2012*7)/6505*10-9)/84*100 AS rank_ed,
-            (AVG(s_emp.2006*1+s_emp.2009*2+s_emp.2012*7)/6505*10-9)/84*100 AS rank_emp,
-            (AVG(s_geo.2006*1+s_geo.2009*2+s_geo.2012*7)/6505*10-9)/84*100 AS rank_geo,
-            (AVG(s_hea.2006*1+s_hea.2009*2+s_hea.2012*7)/6505*10-9)/84*100 AS rank_hea,
-            (AVG(s_hou.2006*1+s_hou.2009*2+s_hou.2012*7)/6505*10-9)/84*100 AS rank_hou,
-            (AVG(s_inc.2006*1+s_inc.2009*2+s_inc.2012*7)/6505*10-9)/84*100 AS rank_inc
+            (AVG(s_cri.2006*1+s_cri.2009*2+s_cri.2012*7)/6505*10-24)/52*100 AS rank_cri,
+            (AVG(s_edu.2006*1+s_edu.2009*2+s_edu.2012*7)/6505*10-18)/74*100 AS rank_edu,
+            (AVG(s_emp.2006*1+s_emp.2009*2+s_emp.2012*7)/6505*10-19)/65*100 AS rank_emp,
+            (AVG(s_geo.2006*1+s_geo.2009*2+s_geo.2012*7)/6505*10-11)/77*100 AS rank_geo,
+            (AVG(s_hea.2006*1+s_hea.2009*2+s_hea.2012*7)/6505*10-17)/63*100 AS rank_hea,
+            (AVG(s_hou.2006*1+s_hou.2009*2+s_hou.2012*7)/6505*10-9)/67*100 AS rank_hou,
+            (AVG(s_inc.2006*1+s_inc.2009*2+s_inc.2012*7)/6505*10-18)/62*100 AS rank_inc
         FROM constituencies c
         LEFT JOIN MSPs m ON c.name=m.constituency
         LEFT JOIN datazones d ON c.id = d.constituency
         LEFT JOIN MSP_words w ON w.MSP_id = m.id
         LEFT JOIN simd_general s_gen on s_gen.datazone = d.code
         LEFT JOIN simd_crime s_cri on s_cri.datazone = d.code
-        LEFT JOIN simd_education s_ed on s_ed.datazone = d.code
+        LEFT JOIN simd_education s_edu on s_edu.datazone = d.code
         LEFT JOIN simd_employment s_emp on s_emp.datazone = d.code
         LEFT JOIN simd_geoacc s_geo on s_geo.datazone = d.code
         LEFT JOIN simd_health s_hea on s_hea.datazone = d.code
@@ -64,6 +76,77 @@ def const_info(constituency):
         LEFT JOIN simd_income s_inc on s_inc.datazone = d.code
         WHERE c.name="{constituency}"
         GROUP BY c.id
+    """.format(constituency=constituency))
+    r = db.use_result()
+    row = r.fetch_row()
+    result = []
+
+    while row:
+        row = row[0]
+        msp = {
+            'MSPfsdfsd_id': row[11],
+            'name': row[0],
+            'surname': row[1],
+            'party': row[2],
+            'url': row[3],
+            'rank_gen': row[4],
+            'total_interventions': row[5],
+            'avg_intervention_len': row[6],
+            'total_mentions_of_constituency': row[7],
+            'interventions_with_mention': row[8],
+            'mentions_percentage_of_total_text': row[9],
+            'percentage_of_interventions_with_mention': row[10],
+            'words': "[{0}]".format(row[12]),
+            'rank_cri': row[13],
+            'rank_edu': row[13],
+            'rank_emp': row[17],
+            'rank_geo': row[14],
+            'rank_hea': row[15],
+            'rank_hou': row[16],
+            'rank_inc': row[18]
+        }
+
+        result += [msp]
+        row = r.fetch_row()
+
+    result = {'result':result}
+    result = jsonify(result)
+
+    # with open("cache/c_"+constituency,"wb") as fo:
+    #     fo.write(result)
+
+    return result
+
+@app.route("/region/<string:constituency>")
+def region_info(constituency):
+    # if checkCache(constituency):
+    #     return app.send_static_file('cahce/' + constituency)
+
+    db.query("""
+        SELECT
+            m.name,
+            m.surname,
+            m.party,
+            m.url,
+            (AVG(s_gen.2006*1+s_gen.2009*2+s_gen.2012*7)/6505*10-18)/67*100 AS rank_gen,
+            m.total_interventions,
+            m.avg_intervention_len,
+            m.total_mentions_of_constituency,
+            m.interventions_with_mention,
+            m.mentions_percentage_of_total_text,
+            m.percentage_of_interventions_with_mention,
+            m.id,
+            GROUP_CONCAT(DISTINCT CONCAT("[\\\"", w.word, "\\\",", w.weight, "]") SEPARATOR ', '),
+            r.region
+        FROM constituencies c
+        LEFT JOIN constituencies_regions r ON c.id = r.id
+        LEFT JOIN MSPs m ON r.region=m.constituency
+        LEFT JOIN MSP_words w ON w.MSP_id = m.id
+        LEFT JOIN constituencies_regions r2 ON c.id = r2.id
+        LEFT JOIN datazones d ON r2.id = d.constituency
+        LEFT JOIN simd_general s_gen on s_gen.datazone = d.code
+        WHERE c.name="{constituency}"
+        GROUP BY m.id
     """.format(constituency=constituency))
     r = db.use_result()
     row = r.fetch_row()
@@ -85,20 +168,19 @@ def const_info(constituency):
             'mentions_percentage_of_total_text': row[9],
             'percentage_of_interventions_with_mention': row[10],
             'words': "[{0}]".format(row[12]),
-            'rank_cri': row[13],
-            'rank_ed': row[13],
-            'rank_emp': row[17],
-            'rank_geo': row[14],
-            'rank_hea': row[15],
-            'rank_hou': row[16],
-            'rank_inc': row[18]
+            'region': row[13]
         }
 
         result += [msp]
         row = r.fetch_row()
 
     result = {'result':result}
-    return jsonify(result)
+    result = jsonify(result)
+
+    # with open("cache/r_"+constituency,"wb") as fo:
+    #     fo.write(result)
+
+    return result
 
 
 @app.route("/stats/", methods=['POST', 'GET'])
@@ -108,46 +190,46 @@ def get_stats():
     fields = {'c_id':'c.id', 'c_name':'c.name'}
 
     if ("rank_gen" in categories) or (len(categories)==0):
-        fields['rank_gen']="(AVG(s_gen.2006*1+s_gen.2009*2+s_gen.2012*7)/6505*10-9)/84*100 AS rank_gen"
+        fields['rank_gen']="(AVG(s_gen.2006*1+s_gen.2009*2+s_gen.2012*7)/6505*10-18)/67*100 AS rank_gen"
         
     if ("rank_cri" in categories) or (len(categories)==0):
-        fields['rank_cri']="(AVG(s_cri.2006*1+s_cri.2009*2+s_cri.2012*7)/6505*10-9)/84*100 AS rank_cri"
+        fields['rank_cri']="(AVG(s_cri.2006*1+s_cri.2009*2+s_cri.2012*7)/6505*10-24)/52*100 AS rank_cri"
         
-    if ("rank_ed" in categories) or (len(categories)==0):
-        fields['rank_ed']="(AVG(s_ed.2006*1+s_ed.2009*2+s_ed.2012*7)/6505*10-9)/84*100 AS rank_ed"
+    if ("rank_edu" in categories) or (len(categories)==0):
+        fields['rank_edu']="(AVG(s_edu.2006*1+s_edu.2009*2+s_edu.2012*7)/6505*10-18)/74*100 AS rank_edu"
         
     if ("rank_emp" in categories) or (len(categories)==0):
-        fields['rank_emp']="(AVG(s_emp.2006*1+s_emp.2009*2+s_emp.2012*7)/6505*10-9)/84*100 AS rank_emp"
+        fields['rank_emp']="(AVG(s_emp.2006*1+s_emp.2009*2+s_emp.2012*7)/6505*10-19)/65*100 AS rank_emp"
         
     if ("rank_geo" in categories) or (len(categories)==0):
-        fields['rank_geo']="(AVG(s_geo.2006*1+s_geo.2009*2+s_geo.2012*7)/6505*10-9)/84*100 AS rank_geo"
+        fields['rank_geo']="(AVG(s_geo.2006*1+s_geo.2009*2+s_geo.2012*7)/6505*10-11)/77*100 AS rank_geo"
         
     if ("rank_gen" in categories) or (len(categories)==0):
-        fields['rank_hea']="(AVG(s_hea.2006*1+s_hea.2009*2+s_hea.2012*7)/6505*10-9)/84*100 AS rank_hea"
+        fields['rank_hea']="(AVG(s_hea.2006*1+s_hea.2009*2+s_hea.2012*7)/6505*10-17)/63*100 AS rank_hea"
         
     if ("rank_hou" in categories) or (len(categories)==0):
-        fields['rank_hou']="(AVG(s_hou.2006*1+s_hou.2009*2+s_hou.2012*7)/6505*10-9)/84*100 AS rank_hou"
+        fields['rank_hou']="(AVG(s_hou.2006*1+s_hou.2009*2+s_hou.2012*7)/6505*10-9)/67*100 AS rank_hou"
         
     if ("rank_inc" in categories) or (len(categories)==0):
-        fields['rank_inc']="(AVG(s_inc.2006*1+s_inc.2009*2+s_inc.2012*7)/6505*10-9)/84*100 AS rank_inc"
+        fields['rank_inc']="(AVG(s_inc.2006*1+s_inc.2009*2+s_inc.2012*7)/6505*10-18)/62*100 AS rank_inc"
 
     if ("total_interventions" in categories) or (len(categories)==0):
-        fields['total_interventions']="(AVG(m.total_interventions)"
+        fields['total_interventions']="AVG(m.total_interventions)"
 
     if ("avg_intervention_len" in categories) or (len(categories)==0):
-        fields['avg_intervention_len']="(AVG(m.avg_intervention_len)"
+        fields['avg_intervention_len']="AVG(m.avg_intervention_len)"
 
     if ("total_mentions_of_constituency" in categories) or (len(categories)==0):
-        fields['total_mentions_of_constituency']="(AVG(m.total_mentions_of_constituency)"
+        fields['total_mentions_of_constituency']="AVG(m.total_mentions_of_constituency)"
 
     if ("interventions_with_mention" in categories) or (len(categories)==0):
-        fields['interventions_with_mention']="(AVG(m.interventions_with_mention)"
+        fields['interventions_with_mention']="AVG(m.interventions_with_mention)"
 
     if ("mentions_percentage_of_total_text" in categories) or (len(categories)==0):
-        fields['mentions_percentage_of_total_text']="(AVG(m.mentions_percentage_of_total_text)"
+        fields['mentions_percentage_of_total_text']="AVG(m.mentions_percentage_of_total_text)"
 
     if ("percentage_of_interventions_with_mention" in categories) or (len(categories)==0):
-        fields['percentage_of_interventions_with_mention']="(AVG(m.percentage_of_interventions_with_mention)"
+        fields['percentage_of_interventions_with_mention']="AVG(m.percentage_of_interventions_with_mention)"
 
     db.query("""
         SELECT
@@ -157,7 +239,7 @@ def get_stats():
         LEFT JOIN datazones d ON c.id = d.constituency
         LEFT JOIN simd_general s_gen on s_gen.datazone = d.code
         LEFT JOIN simd_crime s_cri on s_cri.datazone = d.code
-        LEFT JOIN simd_education s_ed on s_ed.datazone = d.code
+        LEFT JOIN simd_education s_edu on s_edu.datazone = d.code
         LEFT JOIN simd_employment s_emp on s_emp.datazone = d.code
         LEFT JOIN simd_geoacc s_geo on s_geo.datazone = d.code
         LEFT JOIN simd_health s_hea on s_hea.datazone = d.code
--- words2db.py
+++ words2db.py
@@ -4,7 +4,8 @@ import _mysql
 with open('data/msps_words.json', 'r') as content_file:
     content = json.loads(content_file.read())
 
-db = _mysql.connect(host="localhost", user="ilwhack14", passwd="hackathon", db="ilwhack14")
+db = _mysql.connect(host="d33028.mysql.zone.ee", user="d33028sa73662", passwd="Hackath0n", db="d33028sd72729")
+
 words =[]
 for person in content:
     q = "INSERT INTO `MSP_words` (`MSP_id`, `word`, `weight`)  VALUES "
